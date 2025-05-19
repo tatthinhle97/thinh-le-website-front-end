63ae8709fa6e09ba25df78a2310442805f0b2fa3
Thinh Le <let9@go.stockton.edu>
2025-05-18 21:37:43 -0400

    Add react-swc

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
+import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [reactRouter(), tailwindcss()],
+  plugins: [react(), reactRouter(), tailwindcss()],
 });
