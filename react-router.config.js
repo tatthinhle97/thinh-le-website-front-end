17bbe34b8e34e9e998ea8c847688fee8f4327bf2
Thinh Le <let9@go.stockton.edu>
2025-05-20 17:04:26 -0400

    Add preload config

--- react-router.config.js
+++ react-router.config.js
@@ -1,5 +1,9 @@
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: false
+  ssr: false,
+  // return a list of URLs to prerender at build time
+  async prerender() {
+    return ['/', '/projects/sale-and-rental-listings']
+  }
 }
